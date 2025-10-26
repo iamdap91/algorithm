cba355214384c6715f009e5034fdf7a3dc345832
3sam3 <iamdap91@gmail.com>
2025-10-26 22:05:21 +0900

    feat: add flatland space station function and test suite

---
+++ src/hackerrank/easy/flatland-space-station.spec.ts
@@ -0,0 +1,30 @@
+describe('template', () => {
+  function flatlandSpaceStations(n: number, stations: number[]) {
+    let maxDistance = 0;
+    for (let i = 0; i < n; i++) {
+      if (stations.includes(i)) {
+        continue;
+      }
+
+      // 각 포지션별 스테이션 최소거리
+      let minDistanceForStation = Number.MAX_SAFE_INTEGER;
+      for (const station of stations) {
+        const distance = Math.abs(i - station);
+        if (distance < minDistanceForStation) {
+          minDistanceForStation = distance;
+        }
+      }
+
+      if (minDistanceForStation > maxDistance) {
+        maxDistance = minDistanceForStation;
+      }
+    }
+
+    return maxDistance;
+  }
+
+  it('solution', () => {
+    const result = flatlandSpaceStations(5, [0, 2]);
+    console.log(result);
+  });
+});
