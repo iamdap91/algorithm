16131f526980f80e706245b6ddeb52e7b0fa9b6b
3sam3 <iamdap91@gmail.com>
2025-10-31 22:43:10 +0900

    feat: add happy ladybug function and test suite

--- src/hackerrank/easy/strange-counter.spec.ts
+++ src/hackerrank/easy/strange-counter.spec.ts
@@ -1,16 +1,21 @@
 describe('strange-counter.spec.ts', () => {
   function strangeCounter(sequence: number): number {
     let multiplier = 0;
+    let remaining = sequence;
+
+    // 해당 sequence가 속한 사이클 찾기
     while (true) {
-      const current = 3 * Math.pow(2, multiplier);
-      if (sequence < current) {
+      const cycleLength = 3 * Math.pow(2, multiplier);
+      if (remaining <= cycleLength) {
         break;
       }
+      remaining -= cycleLength;
       multiplier++;
-      sequence -= current;
     }
 
-    return sequence;
+    // 현재 사이클의 길이에서 남은 위치를 빼서 값 계산
+    const cycleLength = 3 * Math.pow(2, multiplier);
+    return cycleLength - remaining + 1;
   }
 
   it('solution', () => {
